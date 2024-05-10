b306deae701d0a911066f7351f0d4d31c446089b
OleksiiRatushnyi <gotagota2004@gmail.com>
2024-05-10 13:53:28 +0300

    Created AVehiclePawn class

---
+++ Source/ProjectA/Public/Vehicle/VehiclePawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "VehiclePawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTA_API AVehiclePawn : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+	
+};
