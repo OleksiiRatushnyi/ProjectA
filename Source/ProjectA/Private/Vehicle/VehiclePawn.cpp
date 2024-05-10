ac5734f0251bbd9a36ba3e40ff19b0cde2bf1464
OleksiiRatushnyi <gotagota2004@gmail.com>
2024-05-10 16:18:33 +0300

    Added base vehicle functionality to AVehiclePawn class

--- Source/ProjectA/Private/Vehicle/VehiclePawn.cpp
+++ Source/ProjectA/Private/Vehicle/VehiclePawn.cpp
@@ -1,5 +1,77 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Vehicle/VehiclePawn.h"
 
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+#include "ChaosWheeledVehicleMovementComponent.h"
+
+AVehiclePawn::AVehiclePawn()
+{
+	InitializeMesh();
+	InitializeSpringArm();
+	InitializeCamera();
+	InitializeMovementComponent();
+}
+
+void AVehiclePawn::InitializeMesh() const
+{
+	GetMesh()->SetSimulatePhysics(true);
+	GetMesh()->SetCollisionProfileName(FName("Vehicle"));
+}
+
+void AVehiclePawn::InitializeSpringArm()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetMesh());
+	SpringArm->TargetArmLength = 600.0f;
+	SpringArm->SocketOffset.Z = 150.0f;
+	SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, 130.0f));
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bEnableCameraRotationLag = true;
+	SpringArm->CameraRotationLagSpeed = 2.0f;
+	SpringArm->CameraLagMaxDistance = 50.0f;
+}
+
+void AVehiclePawn::InitializeCamera()
+{
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->AttachToComponent(SpringArm, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+void AVehiclePawn::InitializeMovementComponent()
+{
+	MovementComponent = CastChecked<UChaosWheeledVehicleMovementComponent>(GetVehicleMovement());
+}
+
+void AVehiclePawn::Tick(float Delta)
+{
+	Super::Tick(Delta);
+	
+	ApplyAngularDamping();
+	CameraUpdate(Delta);
+}
+
+void AVehiclePawn::ApplyAngularDamping()
+{
+	// add some angular damping if the vehicle is in midair
+	bool bMovingOnGround = MovementComponent->IsMovingOnGround();
+	GetMesh()->SetAngularDamping(bMovingOnGround ? 0.0f : 3.0f);
+}
+
+void AVehiclePawn::CameraUpdate(float Delta)
+{
+	float CameraYaw = SpringArm->GetRelativeRotation().Yaw;
+	static float TargetYaw = 0.0f;
+	CameraYaw = FMath::FInterpTo(CameraYaw, TargetYaw, Delta, 4.0f);
+	SpringArm->SetRelativeRotation(FRotator(0.0f, CameraYaw, 0.0f));
+}
+
+float AVehiclePawn::GetCurrentSpeed()
+{
+	return MovementComponent->GetForwardSpeed() * 0.036;
+}
--- Source/ProjectA/Public/Vehicle/VehiclePawn.h
+++ Source/ProjectA/Public/Vehicle/VehiclePawn.h
@@ -6,12 +6,40 @@
 #include "WheeledVehiclePawn.h"
 #include "VehiclePawn.generated.h"
 
-/**
- * 
+class USpringArmComponent;
+class UCameraComponent;
+
+class UChaosWheeledVehicleMovementComponent;
+
+/*
+ * Base class for a vehicle
  */
 UCLASS()
 class PROJECTA_API AVehiclePawn : public AWheeledVehiclePawn
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Common Vehicle Functionality
+	AVehiclePawn();
+	void InitializeMesh() const;
+	void InitializeSpringArm();
+	void InitializeCamera();
+	void InitializeMovementComponent();
+
+	virtual void Tick(float Delta) override;
+	void ApplyAngularDamping();
+	void CameraUpdate(float Delta);
+
+	float GetCurrentSpeed();
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UCameraComponent> Camera;
+
+private:
+	TObjectPtr<UChaosWheeledVehicleMovementComponent> MovementComponent;
 };
