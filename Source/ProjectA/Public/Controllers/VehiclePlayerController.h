7a40e909bbea3f7e3f2a964ecd6ead7a0c24b2e4
OleksiiRatushnyi <gotagota2004@gmail.com>
2024-05-10 19:47:35 +0300

    Added input mapping context on pawn possession

--- Source/ProjectA/Public/Controllers/VehiclePlayerController.h
+++ Source/ProjectA/Public/Controllers/VehiclePlayerController.h
@@ -6,12 +6,18 @@
 #include "GameFramework/PlayerController.h"
 #include "VehiclePlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+
 UCLASS()
 class PROJECTA_API AVehiclePlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void OnPossess(APawn* InPawn) override;
+	void AddInputMappingContext();
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputMappingContext> InputMappingContext;
 };
