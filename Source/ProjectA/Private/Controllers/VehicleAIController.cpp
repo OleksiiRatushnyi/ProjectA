a738715b31c8c77b453beaaf2d3ddbc3f971d45c
OleksiiRatushnyi <gotagota2004@gmail.com>
2024-05-11 10:40:04 +0300

    Implemented AI controller functions for steering, throttle, and brake control

--- Source/ProjectA/Private/Controllers/VehicleAIController.cpp
+++ Source/ProjectA/Private/Controllers/VehicleAIController.cpp
@@ -4,6 +4,7 @@
 
 #include "Components/SplineComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 
 DEFINE_LOG_CATEGORY(LogAIController);
 
@@ -13,6 +14,7 @@ void AVehicleAIController::OnPossess(APawn* InPawn)
 
 	InitializeControlledVehicle();
 	InitializePath();
+	CalculateStartingSideOfRoad();
 }
 
 void AVehicleAIController::InitializeControlledVehicle()
@@ -36,4 +38,108 @@ void AVehicleAIController::InitializePath()
 	{
 		UE_LOG(LogAIController, Warning, TEXT("Failed to find BP_AIPath actor"));
 	}
+}
+
+int AVehicleAIController::CalculateStartingSideOfRoad()
+{
+	const float LeftSideDistance = FVector::Distance(GetClosestLocationToPath(ControlledVehicle->GetActorLocation(),
+		0.0f, 0), ControlledVehicle->GetActorLocation());
+	const float RightSideDistance = FVector::Distance(GetClosestLocationToPath(ControlledVehicle->GetActorLocation(),
+		0.0f, 1), ControlledVehicle->GetActorLocation());
+	return LeftSideDistance > RightSideDistance ? SideOfRoad = 1 : SideOfRoad = 0;
+}
+
+FVector AVehicleAIController::GetClosestLocationToPath(const FVector& AILocation, float AdditionalDistance,
+                                                       int32 SideOfRoadParam) const
+{
+	// Checking for the presence of the USplineComponent component
+	if (Path)
+	{
+		const int SideOffset = SideOfRoadParam ? 500 : -500;
+		const float Distance = Path->GetDistanceAlongSplineAtLocation(AILocation, ESplineCoordinateSpace::World);
+		const FVector RightVector = Path->GetRightVectorAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+		return Path->GetLocationAtDistanceAlongSpline(Distance + AdditionalDistance, ESplineCoordinateSpace::World) + RightVector * SideOffset;
+	}
+	
+	UE_LOG(LogAIController, Warning, TEXT("Failed to find USplineComponent"));
+	return FVector::ZeroVector; // Return a default value (zero vector) if Path is null
+}
+
+void AVehicleAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	ControlledVehicle->OnSteeringAction(CalculateSteering());
+	auto [Throttle, Brake] = CalculateThrottleAndBrake(CalculateTopSpeed());
+	ControlledVehicle->OnThrottleAction(Throttle);
+	ControlledVehicle->OnBrakeAction(Brake);
+}
+
+float AVehicleAIController::CalculateSteering() const
+{
+	const float AdditionalDistance = GetAdditionalDistanceForSteering(ControlledVehicle->GetAISettings().SpeedRangeForSteering,
+		ControlledVehicle->GetAISettings().DistanceRangeForSteering);
+	const FRotator ClosestPointToTheSpline = GetClosestPointToTheSpline(AdditionalDistance);
+	const FRotator AngleOfSteering = GetAngleOfSteering(ClosestPointToTheSpline);
+	return GetSteeringInput(ControlledVehicle->GetAISettings().AngleRangeForSteering, ControlledVehicle->GetAISettings().SteeringInputRange,
+		AngleOfSteering);
+}
+
+float AVehicleAIController::GetAdditionalDistanceForSteering(FVector2D SpeedRangeForSteering,
+	FVector2D DistanceRangeForSteering) const
+{
+	return FMath::GetMappedRangeValueClamped(SpeedRangeForSteering, DistanceRangeForSteering,
+		ControlledVehicle->GetCurrentSpeed());
+}
+
+FRotator AVehicleAIController::GetClosestPointToTheSpline(float AdditionalDistance) const
+{
+	return UKismetMathLibrary::FindLookAtRotation(ControlledVehicle->GetFrontOfVehicle(),
+	GetClosestLocationToPath(ControlledVehicle->GetFrontOfVehicle(), AdditionalDistance, SideOfRoad));
+}
+
+FRotator AVehicleAIController::GetAngleOfSteering(const FRotator& ClosestPointToTheSpline) const
+{
+	return UKismetMathLibrary::NormalizedDeltaRotator(ClosestPointToTheSpline, ControlledVehicle->GetActorRotation());
+}
+
+float AVehicleAIController::GetSteeringInput(FVector2D AngleRangeForSteering, FVector2D SteeringInputRange,
+	const FRotator& AngleOfSteering)
+{
+	return FMath::GetMappedRangeValueClamped(AngleRangeForSteering, SteeringInputRange, AngleOfSteering.Yaw);
+}
+
+std::pair<float, float> AVehicleAIController::CalculateThrottleAndBrake(float TopSpeed) const
+{
+	return (ControlledVehicle->GetCurrentSpeed() >= TopSpeed) ? std::make_pair(0.0f, CalculateBrakeIntensity(CalculateTopSpeed())) : std::make_pair(1.0f, 0.0f);
+}
+
+float AVehicleAIController::CalculateBrakeIntensity(float TopSpeed) const
+{
+	const FVector2D SpeedRange(TopSpeed + 1.0f, TopSpeed + 5.0f);
+	const FVector2D IntensityRange(0.0f, 1.0f);
+	return FMath::GetMappedRangeValueClamped(SpeedRange, IntensityRange, ControlledVehicle->GetCurrentSpeed());
+}
+
+float AVehicleAIController::CalculateTopSpeed() const
+{
+	const float AdditionalDistance = GetAdditionalDistanceForTopSpeed(ControlledVehicle->GetAISettings().SpeedRangeForTopSpeed,
+		ControlledVehicle->GetAISettings().DistanceRangeForTopSpeed);
+	const FRotator ClosestPointToTheSpline = GetClosestPointToTheSpline(AdditionalDistance);
+	const FRotator AngleOfSteering = GetAngleOfSteering(ClosestPointToTheSpline);
+	return GetTopSpeed(ControlledVehicle->GetAISettings().AngleRangeForTopSpeed, ControlledVehicle->GetAISettings().TopSpeedRange, AngleOfSteering);
+}
+
+float AVehicleAIController::GetAdditionalDistanceForTopSpeed(FVector2D SpeedRangeForTopSpeed,
+	FVector2D DistanceRangeForTopSpeed) const
+{
+	return FMath::GetMappedRangeValueClamped(SpeedRangeForTopSpeed, DistanceRangeForTopSpeed,
+		ControlledVehicle->GetCurrentSpeed());
+}
+
+float AVehicleAIController::GetTopSpeed(FVector2D AngleRangeForTopSpeed, FVector2D TopSpeedRange,
+	const FRotator& AngleOfSteering)
+{
+	return FMath::GetMappedRangeValueClamped(AngleRangeForTopSpeed, TopSpeedRange,
+		FMath::Abs(AngleOfSteering.Yaw));
 }
--- Source/ProjectA/Public/Controllers/VehicleAIController.h
+++ Source/ProjectA/Public/Controllers/VehicleAIController.h
@@ -20,6 +20,21 @@ public:
 	virtual void OnPossess(APawn* InPawn) override;
 	void InitializeControlledVehicle();
 	void InitializePath();
+	int CalculateStartingSideOfRoad();
+
+	FVector GetClosestLocationToPath(const FVector& AILocation, float AdditionalDistance, int32 SideOfRoadParam) const;
+
+	virtual void Tick(float DeltaSeconds) override;
+	float CalculateSteering() const;
+	float GetAdditionalDistanceForSteering(FVector2D SpeedRangeForSteering, FVector2D DistanceRangeForSteering) const;
+	FRotator GetClosestPointToTheSpline(float AdditionalDistance) const;
+	FRotator GetAngleOfSteering(const FRotator& ClosestPointToTheSpline) const;
+	static float GetSteeringInput(FVector2D AngleRangeForSteering, FVector2D SteeringInputRange, const FRotator& AngleOfSteering);
+	std::pair<float, float> CalculateThrottleAndBrake(float TopSpeed) const;
+	float CalculateBrakeIntensity(float TopSpeed) const;
+	float CalculateTopSpeed() const;
+	float GetAdditionalDistanceForTopSpeed(FVector2D SpeedRangeForTopSpeed, FVector2D DistanceRangeForTopSpeed) const;
+	static float GetTopSpeed(FVector2D AngleRangeForTopSpeed, FVector2D TopSpeedRange, const FRotator& AngleOfSteering);
 	
 private:
 	TWeakObjectPtr<AVehiclePawn> ControlledVehicle;
@@ -28,4 +43,6 @@ private:
 	TSubclassOf<AActor> Spline;
 
 	TObjectPtr<USplineComponent> Path;
+	
+	int32 SideOfRoad;
 };
