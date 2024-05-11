bd8ec7933313875828db9830bf6bf5a19328816f
OleksiiRatushnyi <gotagota2004@gmail.com>
2024-05-11 10:19:14 +0300

    Created FAISettings structure

--- Source/ProjectA/Public/Vehicle/VehiclePawn.h
+++ Source/ProjectA/Public/Vehicle/VehiclePawn.h
@@ -37,6 +37,39 @@ struct FInputActions
 };
 
 /*
+ * The FAISettings structure contains settings for controlling AI
+ */
+USTRUCT()
+struct FAISettings
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "Steering")
+	FVector2D AngleRangeForSteering = FVector2D(-10.0f, 10.0f);
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Steering")
+	FVector2D SteeringInputRange = FVector2D(-1.0f, 1.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Steering ")
+	FVector2D SpeedRangeForSteering = FVector2D(50.0f, 150.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Steering")
+	FVector2D DistanceRangeForSteering = FVector2D(1000.0f, 3000.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Speed")
+	FVector2D AngleRangeForTopSpeed = FVector2D(0.0f, 40.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Speed")
+	FVector2D TopSpeedRange = FVector2D(150.0f, 50.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Speed")
+	FVector2D SpeedRangeForTopSpeed = FVector2D(50.0f, 150.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Speed")
+	FVector2D DistanceRangeForTopSpeed = FVector2D(3000.0f, 9000.0f);
+};
+
+/*
  * Base class for a vehicle
  */
 UCLASS()
@@ -66,6 +99,9 @@ public:
 	void OnHandbrakeAction(const FInputActionValue& Value);
 	void OnSteeringAction(const FInputActionValue& Value);
 
+	// Action processing for AI
+	FAISettings GetAISettings() { return AISettings; }
+	
 protected:
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<USpringArmComponent> SpringArm;
@@ -78,4 +114,7 @@ private:
 
 	UPROPERTY(EditDefaultsOnly)
 	FInputActions InputActions;
+
+	UPROPERTY(EditDefaultsOnly)
+	FAISettings AISettings;
 };
