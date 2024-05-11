eb6253f9f605438770615502f6839fbac5ad198a
OleksiiRatushnyi <gotagota2004@gmail.com>
2024-05-11 10:14:26 +0300

    Added initialization of controlled vehicle and path for AI controller

--- Source/ProjectA/Private/Controllers/VehicleAIController.cpp
+++ Source/ProjectA/Private/Controllers/VehicleAIController.cpp
@@ -1,5 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Controllers/VehicleAIController.h"
 
+#include "Components/SplineComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+DEFINE_LOG_CATEGORY(LogAIController);
+
+void AVehicleAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	InitializeControlledVehicle();
+	InitializePath();
+}
+
+void AVehicleAIController::InitializeControlledVehicle()
+{
+	ControlledVehicle = Cast<AVehiclePawn>(GetPawn());
+}
+
+void AVehicleAIController::InitializePath()
+{
+	if (const AActor* SplineActor = UGameplayStatics::GetActorOfClass(GetWorld(), Spline))
+	{
+		Path = SplineActor->FindComponentByClass<USplineComponent>();
+
+		// Checking for the presence of the USplineComponent component
+		if (!Path)
+		{
+			UE_LOG(LogAIController, Warning, TEXT("Failed to find USplineComponent"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogAIController, Warning, TEXT("Failed to find BP_AIPath actor"));
+	}
+}
--- Source/ProjectA/Public/Controllers/VehicleAIController.h
+++ Source/ProjectA/Public/Controllers/VehicleAIController.h
@@ -4,14 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Vehicle/VehiclePawn.h"
 #include "VehicleAIController.generated.h"
 
-/**
- * 
- */
+DECLARE_LOG_CATEGORY_EXTERN(LogAIController, Warning, All);
+
+class USplineComponent;
+
 UCLASS()
 class PROJECTA_API AVehicleAIController : public AAIController
 {
 	GENERATED_BODY()
+
+public:
+	virtual void OnPossess(APawn* InPawn) override;
+	void InitializeControlledVehicle();
+	void InitializePath();
 	
+private:
+	TWeakObjectPtr<AVehiclePawn> ControlledVehicle;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Path")
+	TSubclassOf<AActor> Spline;
+
+	TObjectPtr<USplineComponent> Path;
 };
